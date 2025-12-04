3263e7d303754cc8db013bbdbe18e4adc1bb5247
Grégoire Seux <gregoire.seux@datadoghq.com>
2025-12-04 11:56:20 +0100

    Slight improvement for day4
    - only iterate on known items instead of the whole grid
    - compute neighbors position only once
    
    Perf moves from 980ms on average to 720ms

--- day4.rb
+++ day4.rb
@@ -34,22 +34,33 @@ end
 # part 1
 puts count
 
+# part2
+
+# make a list of all existing items
+require 'set'
+known_items = Set.new
+grid.each_with_index do |row, x|
+  row.each_with_index do |el, y|
+    known_items.add([x,y]) if el == '@'
+  end
+end
+removed_items = Set.new
+
 count = 0
 should_continue = true
 while should_continue do
   should_continue = false
   to_remove = []
-  grid.each_with_index do |row, x|
-    row.each_with_index do |el, y|
-      next if el != '@'
-      next if paper_neighbors(grid, x,y) >= 4
-      should_continue = true
-      to_remove << [x,y]
-    end
+  known_items.each do |x,y|
+    next if paper_neighbors(grid, x,y) >= 4
+
+    should_continue = true
+    to_remove << [x,y]
   end
   count += to_remove.size
   # now removing them
   to_remove.each do |x,y|
+    known_items.delete([x,y])
     grid[x][y] = '.'
   end
 end
